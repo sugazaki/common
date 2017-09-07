b97cbe769392f98b0dc329d45e0082c05d2e2b07
caiyusong <caiyusong@vivo.com>
2017-09-07 19:17:49 +0800

    update

---
+++ src/main/java/filter/RequestParamFilter.java
@@ -0,0 +1,28 @@
+package filter;
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Created by caiyusong on 2017/9/2.
+ */
+@WebFilter(urlPatterns = "/*")
+public class RequestParamFilter implements Filter{
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        filterChain.doFilter(new CustomHttpServletWrapper((HttpServletRequest)servletRequest),servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
